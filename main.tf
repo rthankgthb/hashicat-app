b86d2822c0428a50fc8219af285f60bc79c959c3
rthankgthb <rickthank@gmail.com>
2023-11-29 22:22:12 +0000

    Added Backup Storage private module

--- main.tf
+++ main.tf
@@ -206,3 +206,8 @@ module "azure-backup" {
   version = "1.0.2"
   resource_group_name = "${var.prefix}-public"
 }
+module "backupstorage" {
+  source = "app.terraform.io/RT-TerraformCloud-Lab/azure-backup/azurerm"
+  version = "1.0.2"
+  resource_group_name = "${var.prefix}-private"
+}
